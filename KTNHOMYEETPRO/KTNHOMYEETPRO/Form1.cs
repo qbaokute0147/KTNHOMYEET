17155be4db7cbba88cb6b7a1800f0a4327718bbd
Chiến <chiendang1011@gmail.com>
2022-03-07 12:58:30 +0700

    B commit

--- KTNHOMYEETPRO/KTNHOMYEETPRO/Form1.cs
+++ KTNHOMYEETPRO/KTNHOMYEETPRO/Form1.cs
@@ -16,5 +16,10 @@ namespace KTNHOMYEETPRO
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
